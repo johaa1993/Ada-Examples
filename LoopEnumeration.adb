519a06a6913addb65c2ad27d6042f11d42a02f4a
Johan <johaa1993@gmail.com>
2014-06-29 09:49:50 +0200

    Create LoopEnumeration.adb

---
+++ LoopEnumeration.adb
@@ -0,0 +1,18 @@
+with Ada.Text_IO;
+procedure Main is
+
+   type Color is (Red, Orange, Yellow, Green, Blue, Violet);
+   
+begin
+
+   for I in Color loop
+      Ada.Text_IO.Put_Line(I'Img);
+   end loop;
+   
+   Ada.Text_IO.New_Line;
+   
+   for I in Color'Range loop
+      Ada.Text_IO.Put_Line(I'Img);
+   end loop;
+   
+end Main;
